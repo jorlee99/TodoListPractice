90474c3dcf243dc34136247b1c35b2b6a795a222
Jordan <jordan@pantheondesign.ca>
2021-08-24 11:51:20 -0700

    Added changes to edit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React , {useState, useEffect, useRef} from 'react' //hooks
 
 function TodoForm(props) {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
 
     const inputRef = useRef(null)
 
@@ -22,9 +22,18 @@ function TodoForm(props) {
         setInput('');
     };
     return (
-        <form className="todo-form" onSubmit={handleSubmit}>
-            <input type ="text" placeholder="Add a To-Do Item" value ={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef}/>
-            <button className="todo-button">Add To-Do</button>
+        <form className="todo-form" onSubmit={handleSubmit}> 
+            {props.edit ?  // if props.edit is true
+            ( <> <input type ="text" placeholder="Add a To-Do Item" value ={input} name='text' className='todo-input-edit' onChange={handleChange} ref={inputRef}/>
+            <button className="todo-button-edit">Update</button> </>) // button that adds todos
+            :
+             (<> <input type ="text" placeholder="Add a To-Do Item" value ={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef}/>
+            <button className="todo-button">Add Todo </button></>)  // button that updates todos
+
+            //doing this allows for seperate stylings for each button in css
+            } 
+            
+            
         </form>
     )
 }
